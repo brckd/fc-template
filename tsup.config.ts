47a63167106a4694b87665ae02846dfa184556d4
Brycked <brycked@proton.me>
2023-04-25 21:59:01 +0200

    chore(build): ignore console calls

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,9 @@ import { polyfillNode } from "esbuild-plugin-polyfill-node";
 
 export default defineConfig({
   esbuildPlugins: [polyfillNode()],
+  esbuildOptions(options) {
+    options.drop = ["console"];
+  },
   tsconfig: "src/tsconfig.json",
   entry: ["src/**/*.ts"],
   clean: true,
